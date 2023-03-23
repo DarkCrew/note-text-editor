1b9870b6176fb8fc16e441ced52411da70105c3f
Kirill Kovalev <kirill.kovalev1502@mail.ru>
2023-03-23 21:12:30 +0300

    add: add ability to edit and delete note

--- my-app/src/components/Note/Note.tsx
+++ my-app/src/components/Note/Note.tsx
@@ -1,3 +1,5 @@
+/* eslint-disable jsx-a11y/no-noninteractive-element-interactions */
+/* eslint-disable jsx-a11y/click-events-have-key-events */
 import React, { ReactElement } from 'react';
 
 import deleteImage from '../../assets/image/delete.png';
@@ -5,14 +7,34 @@ import editImage from '../../assets/image/edit.png';
 
 import styles from './Note.module.scss';
 
-const Note = (): ReactElement => {
+type Props = {
+  id: number;
+  title: string;
+  deleteNote: (id: number) => void;
+  editNode: (id: number, result: string) => void;
+};
+
+const Note = ({ id, title, deleteNote, editNode }: Props): ReactElement => {
+  const changeTitle = (): void => {
+    const result = prompt('Change your note', title);
+    if (result !== title) {
+      editNode(id, String(result));
+    }
+  };
+
   return (
     <div className={styles.note}>
       <p className={styles.title}>Note</p>
-      <p>This text about my first dream</p>
+      <p>{title}</p>
       <div className={styles.menu}>
-        <img src={editImage} alt="edit" />
-        <img src={deleteImage} alt="delete" />
+        <img src={editImage} alt="edit" onClick={changeTitle} />
+        <img
+          src={deleteImage}
+          alt="delete"
+          onClick={(): void => {
+            deleteNote(id);
+          }}
+        />
       </div>
     </div>
   );
--- my-app/src/pages/HomePage/HomePage.tsx
+++ my-app/src/pages/HomePage/HomePage.tsx
@@ -1,29 +1,78 @@
+/* eslint-disable no-param-reassign */
+/* eslint-disable @typescript-eslint/no-use-before-define */
 /* eslint-disable jsx-a11y/control-has-associated-label */
-import React, { ReactElement } from 'react';
+import React, { ChangeEvent, ReactElement } from 'react';
 
 import Note from '../../components/Note/Note';
 
 import styles from './HomePage.module.scss';
 
-const createNote = (): void => {
-  console.log('create Note!!');
+type dataNote = {
+  id: number;
+  title: string;
 };
 
 const HomePage = (): ReactElement => {
+  const [data, setData] = React.useState<dataNote[]>([]);
+  React.useEffect(() => {
+    if (localStorage.getItem('notedata') !== null) {
+      const localStorageData: dataNote[] = JSON.parse(localStorage.getItem('notedata') as string);
+      setData(localStorageData);
+    }
+  }, []);
+
+  React.useEffect(() => {
+    if (data.length !== 0) {
+      localStorage.setItem('notedata', JSON.stringify(data));
+    }
+  }, [data]);
+
+  const [noteCreate, setNoteCreate] = React.useState('');
+  const handleInputChange = (event: ChangeEvent<HTMLInputElement>): void => {
+    setNoteCreate(event.target.value);
+  };
+
+  const createNote = (event: React.MouseEvent<HTMLElement, MouseEvent>): void => {
+    event.preventDefault();
+    if (noteCreate !== '') {
+      const newNote = { id: +Date.now(), title: noteCreate };
+      setData([...data, newNote]);
+      setNoteCreate('');
+    }
+  };
+
+  const deleteNote = (id: number): void => {
+    data.forEach((elem, index) => {
+      if (elem.id === id) {
+        data.splice(index, 1);
+      }
+    });
+    setData([...data]);
+  };
+
+  const editNode = (id: number, result: string): void => {
+    data.forEach((elem) => {
+      if (elem.id === id) {
+        elem.title = result;
+      }
+    });
+    setData([...data]);
+  };
+
   return (
     <section className={styles.home}>
       <div className="container">
         <div className={styles.container}>
           <div className={styles.taskContainer}>
             <p>Create your note</p>
-            <input type="text" />
-            <button type="button" onClick={createNote}>
-              Create
-            </button>
+            <form>
+              <input type="text" name="note" value={noteCreate} onChange={handleInputChange} />
+              <input type="submit" value="Create" onClick={createNote} />
+            </form>
           </div>
           <div className={styles.mainContainer}>
-            {[1, 2, 3].map((elem) => (
-              <Note />
+            {data.map((elem) => (
+              <Note id={elem.id} title={elem.title} deleteNote={deleteNote} editNode={editNode} />
             ))}
           </div>
         </div>
